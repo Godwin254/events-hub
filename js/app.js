dd53457d87a1b78f45750cd37fadeaed6eeef7a0
Godwin254 <godwinbee15@gmail.com>
2021-06-30 23:53:22 +0300

    new commits
    created the sign up page
    
    modified files

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
 (function(){
+    //console.log("CONNECTED!!");
+
+    let disBtn = document.getElementById("disc");
+    let signup = document.getElementById("signup");
+
+    console.log(signup);
+
+    disBtn.addEventListener('click',(e) => {
+        e.preventDefault();
+
+        console.log("CONNECTED!!");
+        window.location.href = "./html/events.html";
+    });
+    //window.alert("Welcome to Events hub");
+
+    //handles click on signup button
+    signup.addEventListener('click', () => {
+        //e.preventDefault();
+        console.log("Sign up");
+        window.location.href = "./html/signup.html";
+
+    });
 
     //variables
 
     //dom-manpulation
 
     //http requests
-})()
+})()//end anonymous function
