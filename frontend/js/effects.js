1db6000e0136cc755351a1b4e93e1d856c517729
Godwin254 <godwinbee15@gmail.com>
2021-07-09 13:48:32 +0300

    added jquery effects to home page

--- frontend/js/effects.js
+++ frontend/js/effects.js
@@ -1,32 +1,47 @@
-
 /********************
-    variables
+1. add effects on the browser
+2. fire events
+3. 
+
+variables
+**********************
 */
 
+$(document).ready(function () {
+  console.log("CONNECTED!");
 
-$(document).ready(() => {
-    console.log("Effects ready");
-    var conttext = $(".cont");
-
-    //conttext.css("background","blue");
-
-    console.log($(".home").height());
-
-
-
-
-
-    //on page scroll
-    $(window).scroll(function(){
-
-        let height = $(document).height();
-
-        if(height > 1030 && height <= 2048){
-            console.log(height);
-            conttext.css({
-                width: "50px"
-            });
-
-        }
-    });
+  //invoke function
+  home();
 });
+
+//animate main page elements
+function home() {
+  console.log("Home effects");
+  let header = $(".banner .home .content > h1");
+  let desc = $(".home .content > h6");
+  let hbtn = $(".home .content > a input");
+
+  hbtn.click(function (e) {
+    e.preventDefault();
+
+    console.log("Clicked home button");
+  });
+
+  hbtn.css({
+    visibility: "hidden",
+  });
+
+  header.animate(
+    {
+      fontSize: "4.8em",
+    },
+    1000,
+    () => {
+      console.log("h1 and h6 animated");
+
+      hbtn.css({
+        visibility: "visible",
+      });
+    }
+  );
+}
